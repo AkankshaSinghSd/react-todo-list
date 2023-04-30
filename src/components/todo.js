4f86e5b5295a97ce09edff2da45bf2f07f227c0b
AkankshaSinghSd <akankshasingh.sd95@gmail.com>
2023-04-30 19:21:29 +0530

    minor changes

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,49 +1,59 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import "../components/todo.css";
-import TodoList from './todoList';
+import TodoList from "./todoList";
 
-const Todo=()=>{
- const [inputList, setInputList] = useState("");
-const [item, setItem] = useState([])
- const itemEvent=(event)=>{
-setInputList(event.target.value);
- };
- const listOfItem=()=>{
-setItem((oldItem)=>{
-return[...oldItem, inputList];
+const Todo = () => {
+  const [inputList, setInputList] = useState("");
+  const [item, setItem] = useState([]);
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+  const listOfItem = () => {
+    setItem((oldItem) => {
+      return [...oldItem, inputList];
+    });
+    setInputList("");
+  };
 
- });
- setInputList("");
-}   
-
-const deletItem=(id)=>{
-setItem((oldItem)=>{
-    return oldItem.filter((arrLi, index)=>{
+  const deletItem = (id) => {
+    setItem((oldItem) => {
+      return oldItem.filter((arrLi, index) => {
         return index !== id;
-    })
-})
-}
+      });
+    });
+  };
 
-
-return(
+  return (
     <>
-    <div className='main_div'>
-    <div className='center_div'>
-        <br />
-        <h1>Todo List</h1>
-        <br />
-        <input type='text' placeholder='Enter the item' value={inputList} onChange={itemEvent} />
-        <button onClick={listOfItem}> +  </button>
-        <ol>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>Todo List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Enter the item"
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button onClick={listOfItem}> + </button>
+          <ol>
             {/* <li>Buy Apple</li> */}
-            {item.map((itemVal,index)=>{
-               return <TodoList text={itemVal} key={index} id={index} onSelect={deletItem}/> 
+            {item.map((itemVal, index) => {
+              return (
+                <TodoList
+                  text={itemVal}
+                  key={index}
+                  id={index}
+                  onSelect={deletItem}
+                />
+              );
             })}
-        </ol>
-    </div>
-    </div>
+          </ol>
+        </div>
+      </div>
     </>
-)
-}
+  );
+};
 
-export default Todo
+export default Todo;
--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "../components/todo.css";
-import DeleteIcon from "../components/delete.svg"
-const TodoList=(props)=>{
-    
-    return (
-        
-        <>
-        <div className="todo-Style">
-        <span className="fa-times"><img height={24} width={24} src={DeleteIcon} onClick={()=>{props.onSelect(props.id)}}/></span>
+import DeleteIcon from "../components/delete.svg";
+const TodoList = (props) => {
+  return (
+    <>
+      <div className="todo-Style">
+        <span className="fa-times">
+          <img
+            height={24}
+            width={24}
+            src={DeleteIcon}
+            onClick={() => {
+              props.onSelect(props.id);
+            }}
+          />
+        </span>
         <li>{props.text}</li>
-         </div>
-        </>
-    ) 
-}
+      </div>
+    </>
+  );
+};
 
-export default TodoList
+export default TodoList;
